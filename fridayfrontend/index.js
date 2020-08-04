cb329ad2e2f8b87f82809fc444305e83379ff8d6
pashsunuwar <psunuwar@qa.com>
2020-08-04 17:52:39 +0100

    sucessfully created a dynamically populated drop down list

---
+++ fridayfrontend/index.js
@@ -0,0 +1,35 @@
+ let dropdown = document.getElementById('ClientMenu')
+ dropdown.length = 0;
+
+ let defaultOption = document.createElement('option');
+ defaultOption.text = 'Choose a client';
+
+ dropdown.add(defaultOption);
+ dropdown.selectedIndex = 0;
+
+ const clientURL="http://localhost:8005/listofclients";
+
+ fetch(clientURL)  
+ .then(  
+   function(response) {  
+     if (response.status !== 200) {  
+       console.warn('Looks like there was a problem. Status Code: ' + response.status);  
+       return;  
+     }
+
+     // Examine the text in the response  
+     response.json().then(function(data) {  
+       console.log(data)
+       let option;
+       
+      for (let i = 0; i<data.length; i++){
+        console.log("client", data[i])
+        option = document.createElement('option');
+        option.text = data[i];
+        dropdown.add(option);
+      }
+    });
+  })
+    .catch(function (error) {
+      console.log("Request failed", error);
+    });
